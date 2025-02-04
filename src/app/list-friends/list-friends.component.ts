e7e926165beddb2f91dc01838f5a2bfbd32e7bfc
sarah <sarah.iglla@gmail.com>
2025-02-04 10:12:06 +0100

    binding event

--- src/app/list-friends/list-friends.component.ts
+++ src/app/list-friends/list-friends.component.ts
@@ -9,7 +9,16 @@ import { OneFriendComponent } from '../one-friend/one-friend.component';
 })
 export class ListFriendsComponent {
   listFriendsAuth : boolean = false
+  listFriendsCreationStatus : string = "aucun ami !"
+  listFriendsTextInput : string = ""
+
   constructor(){
     setTimeout(()=>{this.listFriendsAuth = true},3000)
+  }
+  onAddingFriends(): void {
+    this.listFriendsCreationStatus = "votre ami a été ajouté";
+  }
+  onUpdateFriendsList(eventInput:Event):void{
+    this.listFriendsTextInput = (<HTMLInputElement>eventInput.target).value;
   }
 }
